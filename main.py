e7b7581a3904272830584c7ad5d9a30ba37324b4
Akshith Hiremath <akshith272727@gmail.com>
2021-08-22 15:32:14 +0530

    pushed to repository

--- main.py
+++ main.py
@@ -34,8 +34,13 @@ test_list = ['Nestle SA', 'Wal-Mart Stores Inc']
 #     else:
 #         print(f"{name} had {ticker.upper()} as ticker symbol")
 #         names_with_ticker.append([name, ticker.upper()])
-
-for name in ['Toronto-Dominion Bank']:
+def get_ticker(name):
+    """
+    Function to encapsulate for loop and execute every time to get ticker symbol
+    Also check whether direct match or indirect, and list all further matches as well
+    """
+    
+for name in names[:50]:
     search_query = f"https://www.google.com/search?q={name.replace(' ','+').replace('(', '').replace(')', '')}+yahoo+finance"
     # print(search_query)
     raw = requests.get(search_query)
@@ -44,55 +49,24 @@ for name in ['Toronto-Dominion Bank']:
     match = False
     for header in headers:
         text = header.string
-        print(text)
+        # print(text)
         # print('__')
         remove_irrelevant_brackets = lambda text: re.sub('\([A-Za-z][a-z]*\)', '', text)
         text = remove_irrelevant_brackets(text)
-        print(text)
+        # print(text)
         match = re.search(r'\((\S*?)\)', text)
         if match == None:
             ticker = False
         else:
             ticker = match.group(1)
-            print('match found')
+            # print('match found')
             break
     if not ticker:
         print(f"{name} had no match")
+        names_with_ticker.append([name, 'undefined'])
     else:
         print(f"{name} had {ticker.upper()} as ticker symbol")
         names_with_ticker.append([name, ticker.upper()])
 
-print(names_with_ticker)
-# print(names_with_ticker)
-# df = pd.DataFrame(names_with_ticker)
-# df.to_csv('output.csv')
-    # print(links[0].attrs['href'])
-# raw = requests.get("https://www.google.com")
-# print(raw.content)
-# soup = BeautifulSoup(raw.content, "lxml")
-# links = soup.findAll("a")
-# for index, link in enumerate(links):
-#     print("link {index}: {link}".format(link=link, index=index+1))
-# names = data['name'].tolist()
-# print(type(names[0]))
-#name, display_name
-# data = pd.read_csv('data.csv')
-# names = []
-# for name in data['name'].tolist():
-#     names.append([name.replace(' ', '+')])
-
-# for index, name in enumerate(data['display_name'].tolist()):
-#     names[index].append(name.replace(' ', '+'))
-
-# test = names[0][0]
-# search_query = f"https://www.google.com/search?q={test}"
-# raw = requests.get(search_query)
-# soup = BeautifulSoup(raw.content, "lxml")
-# links = soup.findAll("a")
-# for index, link in enumerate(links):
-#     print(f"link {index}: {link}")
-# for name, display_name in names:
-#     search_query = f"https://www.google.com/search?q={name}"
-#     raw = requests.get(search_query)
-#     soup = BeautifulSoup(raw.content, "lxml")
-#     links = soup.findAll("a")
+df = pd.DataFrame(names_with_ticker)
+df.to_csv('output.csv')
