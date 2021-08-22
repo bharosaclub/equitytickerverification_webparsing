a51d4dbb8bdc3567328c73cb646e0957611e6bd2
Akshith Hiremath <akshith272727@gmail.com>
2021-08-22 12:36:57 +0530

    working for indirect matches+error catching

--- main.py
+++ main.py
@@ -12,29 +12,56 @@ names_with_ticker = []
 # print (names)
 # test_list = [names[0]]
 test_list = ['Nestle SA', 'Wal-Mart Stores Inc']
-for name in ['Quantum Information Services']:
-    search_query = f"https://www.google.com/search?q={name.replace(' ','+')}+yahoo+finance"
+# for name in ['Quantum Information Services']:
+#     search_query = f"https://www.google.com/search?q={name.replace(' ','+')}+yahoo+finance"
+#     raw = requests.get(search_query)
+#     soup = BeautifulSoup(raw.text, "lxml")
+#     links = soup.findAll("a")
+#     match = False
+#     for link in links:
+#         # print(link.attrs['href'])
+#         match = re.search(r'\w?\w?.?finance.yahoo.com/(\w*?)?/?(\w*)', link.attrs['href'], flags = 0)
+#         if match == None:
+#             ticker = False
+#         else:
+#             ticker = match.group(2)
+#             # print(f"{name} had {match.group(2)} as ticker symbol")
+#             break
+#     if not ticker:
+#         print(f"{name} had no match")
+#         ticker = "none"
+#         names_with_ticker.append([name, ticker])
+#     else:
+#         print(f"{name} had {ticker.upper()} as ticker symbol")
+#         names_with_ticker.append([name, ticker.upper()])
+for name in ['Toronto-Dominion Bank']:
+    search_query = f"https://www.google.com/search?q={name.replace(' ','+').replace('(', '').replace(')', '')}+yahoo+finance"
+    # print(search_query)
     raw = requests.get(search_query)
     soup = BeautifulSoup(raw.text, "lxml")
-    links = soup.findAll("a")
+    headers = soup.findAll("h3")
     match = False
-    for link in links:
-        # print(link.attrs['href'])
-        match = re.search(r'\w?\w?.?finance.yahoo.com/(\w*?)?/?(\w*)', link.attrs['href'], flags = 0)
+    for header in headers:
+        text = header.string
+        print(text)
+        # print('__')
+        remove_irrelevant_brackets = lambda text: re.sub('\([A-Z][a-z]*\)', '', text)
+        text = remove_irrelevant_brackets(text)
+        print(text)
+        match = re.search(r'\((\S*?)\)', text)
         if match == None:
             ticker = False
         else:
-            ticker = match.group(2)
-            # print(f"{name} had {match.group(2)} as ticker symbol")
+            ticker = match.group(1)
+            print('match found')
             break
     if not ticker:
         print(f"{name} had no match")
-        ticker = "none"
-        names_with_ticker.append([name, ticker])
     else:
         print(f"{name} had {ticker.upper()} as ticker symbol")
         names_with_ticker.append([name, ticker.upper()])
 
+print(names_with_ticker)
 # print(names_with_ticker)
 # df = pd.DataFrame(names_with_ticker)
 # df.to_csv('output.csv')
